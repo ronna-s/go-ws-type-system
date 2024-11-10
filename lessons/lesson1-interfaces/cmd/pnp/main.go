1f4203639d53e5e4baa5edb33cfa450cad1d886d
Ronna Steinberg <ronna@upvest.co>
2024-11-10 23:58:52 +0100

    remove implementaion

--- lessons/lesson1-interfaces/cmd/pnp/main.go
+++ lessons/lesson1-interfaces/cmd/pnp/main.go
@@ -3,10 +3,9 @@ package main
 import (
 	"github.com/ronna-s/go-design-workshop/lessons/lesson1-interfaces/pkg/pnp"
 	"github.com/ronna-s/go-design-workshop/lessons/lesson1-interfaces/pkg/pnp/engine/tview"
-	"github.com/ronna-s/go-design-workshop/lessons/lesson1-interfaces/pkg/pnpdev"
 )
 
 func main() {
-	game := pnp.New(pnpdev.NewRubyist())
+	game := pnp.New()
 	game.Run(engine.New())
 }
